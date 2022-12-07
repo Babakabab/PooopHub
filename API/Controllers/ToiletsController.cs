7525a4a87d6bbb09ceebb7890fabd4d234ce1782
Babak Jahangiri <babak.jahangiri@jooble.com>
2022-12-07 08:16:38 +0300

    feat: Added Delete.cs

--- API/Controllers/ToiletsController.cs
+++ API/Controllers/ToiletsController.cs
@@ -33,4 +33,10 @@ public class ToiletsController:BaseApiController
         toilet.Id = id;
         return Ok(await Mediator.Send(new Edit.Command{Toilet=toilet}));
     }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteToilet(Guid id)
+    {
+        return Ok(await Mediator.Send(new Delete.Command { Id = id }));
+    }
 }
--- Application/Toilets/Delete.cs
+++ Application/Toilets/Delete.cs
@@ -1,35 +1,29 @@
-using AutoMapper;
-using Domain;
 using MediatR;
 using Persistance;
 
 namespace Application.Toilets;
 
-public class Edit
+public class Delete
 {
     public class Command : IRequest
     {
-        public Toilet Toilet { get; set; }
+        public Guid Id { get; set; }
     }
 
     public class Handler : IRequestHandler<Command>
     {
         private readonly DataContext _context;
-        private readonly IMapper _mapper;
 
-        public Handler(DataContext context, IMapper mapper)
+        public Handler(DataContext context)
         {
             _context = context;
-            _mapper = mapper;
         }
         public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
         {
-            var toilet = await _context.Toilets.FindAsync(request.Toilet.Id);
-            _mapper.Map(request.Toilet, toilet);
+            var toilet = await _context.Toilets.FindAsync(request.Id);
+            _context.Toilets.Remove(toilet);
             await _context.SaveChangesAsync();
-
             return Unit.Value;
         }
     }
-
 }
